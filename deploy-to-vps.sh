7f7bff033dfc00991ac042e054cd3a51e458f0c8
neocxf <neocxf@gmail.com>
2018-07-10 16:55:04 +0800

    add support for vps

--- deploy-to-vps.sh
+++ deploy-to-vps.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+kill -9 $(lsof -t -i:3000)
 
 rm -rf /root/runner/auth
 
-mkdir /root/runner/auth
+git clone https://github.com/neocxf/koa-ts-sqlite /root/runner/auth
 
 cd /root/runner/auth
+
+npm install
+
+export NODE_ENV=development npm run prod
 
